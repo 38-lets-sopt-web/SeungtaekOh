1a25b153b59fa131bc9f87dda250aca7202db459
winchoose <tmdxor741@naver.com>
2026-05-08 00:41:27 +0900

    feat: 로그인 api 연동

--- week4/Homework/src/pages/signinPage/signinPage.tsx
+++ week4/Homework/src/pages/signinPage/signinPage.tsx
@@ -1,13 +1,39 @@
 import { useState } from "react";
+import { useNavigate } from "react-router";
+
+import { signIn } from "../../shared/api/auth";
 import Button from "../../shared/components/button/button";
 import Input from "../../shared/components/input/input";
-import * as styles from "./singinPage.css";
 import { hasEmptyValue } from "../../shared/utils/validators";
+import * as styles from "./singinPage.css";
 
 const SigninPage = () => {
+  const navigate = useNavigate();
   const [loginId, setLoginId] = useState("");
   const [password, setPassword] = useState("");
   const isLoginButtonDisabled = hasEmptyValue(loginId, password);
+
+  const handleSignin = async () => {
+    try {
+      const response = (await signIn({ loginId, password })) as {
+        data?: {
+          userId?: number;
+        };
+      };
+
+      const userId = response.data?.userId;
+
+      if (!userId) {
+        throw new Error("userId가 없습니다.");
+      }
+
+      localStorage.setItem("userId", String(userId));
+      navigate("/mypage");
+    } catch {
+      alert("로그인에 실패했습니다.");
+    }
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>SOPT MEMBERS</h1>
@@ -28,8 +54,9 @@ const SigninPage = () => {
       <Button
         text="로그인"
         guideText="이미 계정이 있나요?"
-        linkText="로그인"
+        linkText="회원가입"
         disabled={isLoginButtonDisabled}
+        onClick={handleSignin}
       />
     </div>
   );
